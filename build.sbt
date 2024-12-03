88c3acfb8ef055be20e3d373810c0e0ff64f38c9
Jan Boerman <jan.boerman@technolution.nl>
2024-12-03 08:00:29 +0100

    Solve Day 3.

--- build.sbt
+++ build.sbt
@@ -2,6 +2,7 @@ lazy val root = project.in(file("."))
     .settings(
         name := "AdventOfCode2024",
         version := "0.1",
-        scalaVersion := "3.6.1",
+        scalaVersion := "3.3.3",
+        libraryDependencies += "com.lihaoyi" %% "fastparse" % "3.1.1",
     )
 
--- src/main/scala/day03/Day03.scala
+++ src/main/scala/day03/Day03.scala
@@ -1,12 +1,72 @@
 package day03
 
+import fastparse.*
+import fastparse.NoWhitespace.*
+import fastparse.given
+
 import scala.io.Source
 
 val source = Source.fromResource("day03.in")
+val input = source.mkString
+
+enum Instruction:
+    case Mul(a: Int, b: Int)
+    case Do
+    case Dont
+import Instruction.*
+
+def parseNum[$: P]: P[Int] =
+    P(CharIn("0-9").rep(min=1, max=3).!.map(_.toInt))
+
+def parseMul[$: P]: P[Mul] =
+    P("mul(" ~ parseNum ~ "," ~ parseNum ~ ")").map { case (x, y) => Mul(x, y) }
+
+def parseDo[$: P]: P[Instruction] =
+    P("do()").!.map(_ => Do)
+
+def parseDont[$: P]: P[Instruction] =
+    P("don't()").!.map(_ => Dont)
+
+// part 1
+def parseMaybeMul[$: P]: P[Mul | Unit] =
+    parseMul | P(AnyChar)
+
+def parseMultiplications[$: P]: P[Seq[Mul]] =
+    parseMaybeMul.rep.map(seq => seq.collect { case x: Mul => x })
+
+def inputMultiplications: Seq[Mul] =
+    val Parsed.Success(pairs, _) = parse(input, parseMultiplications): @unchecked
+    pairs
+
+def solve1(multiplications: Seq[Mul]): Int = multiplications.map { case Mul(x, y) => x * y }.sum
+
+// part 2
+def parseInstruction[$: P]: P[Instruction | Unit] =
+    parseMul | parseDo | parseDont | P(AnyChar)
+
+def parseInstrunctions[$: P]: P[Seq[Instruction]] =
+    parseInstruction.rep.map(seq => seq.collect { case x: Instruction => x })
+
+def inputInstructions: Seq[Instruction] =
+    val Parsed.Success(instructions, _) = parse(input, parseInstrunctions): @unchecked
+    instructions
+
+def solve2(instructions: Seq[Instruction]): Int =
+    var enabled = true
+    var sum = 0
+    for ins <- instructions do
+        ins match
+            case Do => enabled = true
+            case Dont => enabled = false
+            case Mul(x, y) => if enabled then sum += x * y
+    sum
 
 @main def main(): Unit = {
 
-    val result1 = ???
+    val result1 = solve1(inputMultiplications)
     println(result1)
+
+    val result2 = solve2(inputInstructions)
+    println(result2)
 
 }
