a20fa53f10fa2b15b0f0c1a21bab9e1431c94a31
Jan Boerman <jan.boerman@technolution.nl>
2024-12-07 00:16:56 +0100

    Solve Day 6 part 2.

--- src/main/scala/day06/Day06.scala
+++ src/main/scala/day06/Day06.scala
@@ -55,7 +55,8 @@ def step(state: State, map: Map): State = {
     State(nextX, nextY, direction)
 }
 
-def isInMap(x: Int, y: Int, map: Map): Boolean = 0 <= y && y < map.size && 0 <= x && x < map(y).size
+def isInMap(x: Int, y: Int, map: Map): Boolean =
+    0 <= y && y < map.size && 0 <= x && x < map(y).size
 
 def collectCoordinatesTilOutOfMap(initialState: State, map: Map): Set[(Int, Int)] = {
     val res = Set.newBuilder[(Int, Int)]
@@ -67,32 +68,53 @@ def collectCoordinatesTilOutOfMap(initialState: State, map: Map): Set[(Int, Int)
     res.result()
 }
 
-def collectStatesTilOutOfMap(initialState: State, map: Map): Set[(Int, Int)] = {
-    val obstaclePositions = Set.newBuilder[(Int, Int)]
+def willGetStuck(initialState: State, map: Map): Boolean = {
     val states = mutable.Set.empty[State]
     var state = initialState
     while isInMap(state.guardX, state.guardY, map) do
-        if states.contains(State(state.guardX, state.guardY, state.direction.turnRight)) then
-            // We've been here before, we went right!
-            val (nextX, nextY) = nextCoordinates(state.guardX, state.guardY, state.direction)
-            // So at the *next* tile we could have an obstacle to force a right turn (and end up in a loop)!
-            if isInMap(nextX, nextY, map) && getTile(nextX, nextY, map) != Tile.Obstacle then
-                // Only register the extra obstacle tile if it's not already an obstacle, and it's not outside the map.
-                obstaclePositions.addOne((nextX, nextY))
-            end if
-        end if
+        if states.contains(state) then
+            return true
         states.addOne(state)
         state = step(state, map)
     end while
-    obstaclePositions.result()
+    false
 }
+
+def extraMaps(map: Map): Seq[Map] =
+    for y <- map.indices; x <- map(y).indices; if getTile(x, y, map) == Tile.Empty yield replaceTile(x, y, map, Tile.Obstacle)
+
+def replaceTile(x: Int, y: Int, map: Map, tile: Tile): Map =
+    map.updated(y, map(y).updated(x, tile))
+
+// Idea which should perform better (implementation is currently bugged though..):
+//def collectStatesTilOutOfMap(initialState: State, map: Map): Set[(Int, Int)] = {
+//    val obstaclePositions = mutable.Set.empty[(Int, Int)]
+//    val states = mutable.Set.empty[State]
+//    var state = initialState
+//    while isInMap(state.guardX, state.guardY, map) do
+//        if states.contains(State(state.guardX, state.guardY, state.direction.turnRight)) then
+//            // We've been here before, we went right!
+//            val (nextX, nextY) = nextCoordinates(state.guardX, state.guardY, state.direction)
+//            // So at the *next* tile we could have an obstacle to force a right turn (and end up in a loop)!
+//            if isInMap(nextX, nextY, map) && getTile(nextX, nextY, map) != Tile.Obstacle then
+//                // Only register the extra obstacle tile if it's not already an obstacle, and it's not outside the map.
+//                obstaclePositions.addOne((nextX, nextY))
+//            end if
+//        end if
+//        states.addOne(state)
+//        state = step(state, map)
+//    end while
+//    obstaclePositions.remove((initialState.guardX, initialState.guardY))
+//    obstaclePositions.toSet
+//}
 
 @main def main(): Unit = {
 
     val result1 = collectCoordinatesTilOutOfMap(initialState(input), input).size
     println(result1)
 
-    val result2 = collectStatesTilOutOfMap(initialState(input), input).size //278 too low
+    //val result2 = collectStatesTilOutOfMap(initialState(input), input).size //278 too low
+    val result2 = extraMaps(input).count(map => willGetStuck(initialState(input), map))
     println(result2)
 
 }
