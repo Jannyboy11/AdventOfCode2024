2a5c9182985a95f538472b3d94f3a4a928ad0fce
Jan Boerman <jan.boerman@technolution.nl>
2024-12-15 13:43:00 +0100

    Solve Day 9, part 1.

--- src/main/scala/day09/Day09.scala
+++ src/main/scala/day09/Day09.scala
@@ -1,122 +1,110 @@
 package day09
 
-import day09.Fragment.FreeSpace
+import day09.Fragment.{File, FreeSpace}
 
 import scala.collection.mutable
 import scala.io.Source
 
+//val testInput = "12345".map(_ - '0').toIndexedSeq
+val testInput = "2333133121414131402".map(_ - '0').toIndexedSeq
+
 val source = Source.fromResource("day09.in")
 val input: Seq[Int] = source.map(_ - '0').toIndexedSeq
 
-//val EMPTY = -1
-//
-//def diskSpace(diskMap: Seq[Int]): Seq[Int] = diskMap.zipWithIndex.flatMap:
-//    case (size, index) => if index % 2 == 0 then Seq.fill(size)(index / 2) else Seq.fill(size)(EMPTY)
-//
-//def defrag1(disk: Seq[Int]): Seq[Int] = {
-//    val array: Array[Int] = disk.toArray
-//    var upperIdx = array.length - 1
-//    var lowerIdx = 0
-//
-//    def moveLowerUp(): Unit = while array(lowerIdx) != EMPTY do lowerIdx += 1
-//    def moveUpperDown(): Unit = while array(upperIdx) == EMPTY do upperIdx -= 1
-//
-//    while upperIdx > {moveLowerUp(); lowerIdx} do
-//        println(java.util.Arrays.toString(array))
-//
-//        array(lowerIdx) = array(upperIdx)
-//        array(upperIdx) = EMPTY
-//        moveUpperDown()
-//    end while
-//
-//    array.toSeq
-//}
-//
-//def filesystemChecksum(disk: Seq[Int]): Int =
-//    disk.takeWhile(_ != EMPTY).zipWithIndex.map(_ * _).sum
-
 type ID = Int
 type BlockSize = Int
-enum Fragment(size: BlockSize):
-    case File(id: ID, size: BlockSize) extends Fragment(size)
-    case FreeSpace(size: BlockSize) extends Fragment(size)
+enum Fragment:
+    case File(index: Int, id: ID, size: BlockSize)
+    case FreeSpace(index: Int, size: BlockSize)
 
 type Disk = Seq[Fragment]
 
-def parseDisk(diskMap: Seq[Int]): Disk = diskMap.zipWithIndex.map:
-    case (size, index) => if index % 2 == 0 then Fragment.File(index / 2, size) else Fragment.FreeSpace(size)
+def parseDisk(diskMap: Seq[Int]): Disk = diskMap.zipWithIndex
+    .map:
+        case (size, index) => if index % 2 == 0 then Fragment.File(index, index / 2, size) else Fragment.FreeSpace(index, size)
+    .filter { case Fragment.FreeSpace(_, 0) => false; case _ => true }
 
-case class File(id: ID, var size: BlockSize)
+// for debugging
+def diskToString(disk: Disk): String = disk.flatMap {
+    case Fragment.FreeSpace(index, size) => ".".repeat(size)
+    case Fragment.File(index, id, size) => s"$id".repeat(size)
+}.mkString
 
-class Slot:
-    val filled: mutable.Buffer[File] = new mutable.ArrayBuffer[File]()
-    var empty: BlockSize = 0
+def defragment1(disk: Disk): Disk = {
+    val deque = scala.collection.mutable.ArrayDeque.from(disk)
 
-    def size: BlockSize = empty + filled.size
-    def isFull: Boolean = empty == 0
-    def isEmpty: Boolean = filled.isEmpty || filled.forall(_.size == 0)
-    def fill(`with`: File): Unit =
-        val sizeToAdd = Math.min(empty, `with`.size)
-        filled.addOne(File(`with`.id, sizeToAdd))
-        `with`.size -= sizeToAdd
-    def firstFile: File = filled.head
+    var insertIndex = 0
+    var removeIndex = deque.size - 1
 
-    override def toString: String = s"Slot(files=$filled,emptySize=$empty)"
+    def moveInsertIndex() : Unit = while !deque(insertIndex).isInstanceOf[Fragment.FreeSpace] do insertIndex += 1
+    def moveRemoveIndex(): Unit = removeIndex = Math.min(removeIndex, deque.size - 1)
 
-def toSlot(fragment: Fragment): Option[Slot] = fragment match
-    case Fragment.File(id, size) => val slot = Slot(); slot.filled.addOne(File(id, size)); Some(slot)
-    case FreeSpace(0) => None
-    case FreeSpace(size) => val slot = Slot(); slot.empty = size; Some(slot)
+    while { moveRemoveIndex(); removeIndex } > { moveInsertIndex(); insertIndex } do
+        deque(removeIndex) match
+            case Fragment.FreeSpace(_, _) =>
+                removeIndex -= 1
+            case file @ Fragment.File(fileIdx, fileId, fileSize) =>
+                val Fragment.FreeSpace(freeSpaceIdx, availableSize) = deque(insertIndex): @unchecked
 
-def toFragments(slot: Slot): Seq[Fragment] =
-    val files = slot.filled.toSeq.map { case File(id, size) => Fragment.File(id, size) }
-    if slot.isFull then files else files :+ Fragment.FreeSpace(slot.empty)
-
-def defrag(disk: Disk): Disk = {
-    val slots = disk.flatMap(toSlot).toArray
-    println(java.util.Arrays.toString(slots.asInstanceOf[Array[Object]]))
-
-    var lowerIdx = 0
-    var upperIdx = slots.length - 1
-
-    def moveLowerUp(): Unit = while slots(lowerIdx).isFull do lowerIdx += 1
-
-    while {moveLowerUp(); lowerIdx} < upperIdx do
-        val lowerSlot = slots(lowerIdx)
-        val upperSlot = slots(upperIdx)
-
-        println(s"lowerSlot=$lowerSlot")
-        println(s"upperSlot=$upperSlot")
-
-        if !upperSlot.isEmpty then
-            assert(!lowerSlot.isFull)
-            val upperSlotFile = upperSlot.firstFile
-            lowerSlot.fill(upperSlotFile)   //TODO why do we keep doing this?
-        end if
-
-        if upperSlot.isEmpty then
-            upperIdx -= 1
-        end if
+                if fileSize < availableSize then
+                    deque.patchInPlace(removeIndex, Seq(), 1)   // not necessary to put a Fragment.FreeSpace back
+                    deque.patchInPlace(insertIndex, Seq(file, Fragment.FreeSpace(freeSpaceIdx, availableSize - fileSize)), 1)
+                else if fileSize == availableSize then
+                    deque.patchInPlace(removeIndex, Seq(), 1)   // not necessary to put a Fragment.FreeSpace back
+                    deque.patchInPlace(insertIndex, Seq(file), 1)
+                    removeIndex -= 1
+                else
+                    deque.patchInPlace(removeIndex, Seq(Fragment.File(fileIdx, fileId, fileSize - availableSize)), 1)
+                    deque.patchInPlace(insertIndex, Seq(Fragment.File(fileIdx, fileId, availableSize)), 1)
+                end if
+        end match
     end while
 
-    slots.flatMap(toFragments)
+    deque.toSeq
 }
 
-//def compress(disk: Disk): Disk = {
-//    ??? // TODO
-//}
+def filesystemChecksum(disk: Disk): Long = {
+    var index = 0
+    var position = 0
+    var sum = 0L
+
+    while index < disk.size do
+        disk(index) match
+            case Fragment.FreeSpace(_, size) =>
+                position += size
+            case Fragment.File(_, id, size) =>
+                var i = 0
+                while i < size do
+                    sum += position * id
+
+                    position += 1
+                    i += 1
+                end while
+        end match
+        index += 1
+    end while
+
+    sum
+}
+
+def defragment2(disk: Disk): Disk = {
+
+    
+
+
+
+
+    ???
+}
 
 @main def main(): Unit = {
 
-    val input = "2333133121414131402".map(_ - '0')
-
     val disk = parseDisk(input)
-    println(disk)
-    val defragged = defrag(disk)
-    println(defragged)
 
-//    val result1 = filesystemChecksum(defrag1(diskSpace(input)))
-    val result1 = ???
-    println(result1) //1523575464 is too low.
+    val result1 = filesystemChecksum(defragment1(disk))
+    println(result1)
+
+    val result2 = filesystemChecksum(defragment2(parseDisk(testInput)))
+    println(result2)
 
 }
